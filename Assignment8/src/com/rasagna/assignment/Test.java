a3900a6e150f0a55ef5066c88ab582f7fd334a50
RasagnaleRasagnalebakula <lebakularasagna1@gmail.com>
2019-10-24 21:28:41 +0530

    Spring Core

---
+++ Assignment8/src/com/rasagna/assignment/Test.java
@@ -0,0 +1,21 @@
+package com.rasagna.assignment;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		ConfigurableApplicationContext context =  new ClassPathXmlApplicationContext(new String[] {"sample.xml"});
+			
+		    	CustomerService cust = (CustomerService)context.getBean("customerService");
+		    	
+		    	System.out.println(cust);
+		    	
+		    	context.close();
+
+	}
+
+}
