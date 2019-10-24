115ff84536786e499b45c6eccf763f01b17e5bcf
Rasagnalebakula <lebakularasagna1@gmail.com>
2019-10-24 20:33:13 +0530

    spring Core Assigment

---
+++ Assignment9/src/com/rasagna/assignment/DemoApp.java
@@ -0,0 +1,21 @@
+package com.rasagna.assignment;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DemoApp 
+{
+	public static void main(String[] args) {
+		 
+        // Reading configuration from the spring configuration file.
+        ConfigurableApplicationContext   context = new ClassPathXmlApplicationContext("sample.xml");
+ 
+        Person myperson = context.getBean("personBean", Person.class);
+ 
+        System.out.println("Name= " + myperson.getName());
+ 
+        // Closing the context object.
+        context.close();
+
+}
+}
